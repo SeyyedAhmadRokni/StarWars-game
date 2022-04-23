627d69ead7c09c843e8a03f6ead37274c46650d7
SeyyedAhmad <ahmadr1381@gmail.com>
2022-04-23 22:58:12 +0430

    GAME_PATH es moved to Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 #include <StarWars.hpp>
 
+const std::string GAME_TITLE = "Star Wars";
+const std::string GAME_PATH = "/home/seyyedahmad/Documents/Term2/Taklif5/Last";
+const std::string ENEMY_IMAGES_PATH = "/img/enemy";
+const std::string PLAYER_IMAGES_PATH = "/img/ship";
 const int FIRE_WIDHT = 2;
 const int FIRE_HEIGHT = 2;
 
